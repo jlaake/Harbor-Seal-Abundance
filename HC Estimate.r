27b2bd67c3ea023d9a89a87f6ebcd229a3a20db0
Jeff Laake <jefflaake@gmail.com>
2020-09-09 11:08:52 -0700

    More comments

--- HC Estimate.r
+++ HC Estimate.r
@@ -6,20 +6,21 @@ HCdata$tidetype=rep("Other",nrow(HCdata))
 HCdata$tidetype[HCdata$Sitecode%in%c(8.04,8.05,8.06,8.08,8.13,8.14,8.15)]="High"
 HCdata$tidetype[HCdata$Sitecode%in%c(8.16,8.19)]="Low"
 HCdata$tidetype=factor(HCdata$tidetype)
-# this is what was used in London et al although not strictly correct since 227 in non leap and 228 in leap years
+# This is what was used in London et al although not strictly correct since 227 in non leap and 228 in leap years
+# it is the decimal months from 15 August
 HCdata$MFAug15 <- (HCdata$Julian- 226)/31
 HCdata$hour = factor(hour(HCdata$Survey.time),levels=0:23)
-# temporary value until I get tides but will be used for sites other than Hi tide sites
-#HCdata$MinFHi=0
+# For non-high tide haulout locations use the peak value for high tide;
+# hopefully this will minimize any positive bias in abundance estimate due to differences.
+# Comment out the following line to examine the effect of this assumption
 HCdata$time_from_high_minutes[HCdata$tidetype!="High"]=65
-# Create categories defined in London et al
+# Create high tide categories defined in London et al
 HCdata$MinFHi.Cat=cut(HCdata$time_from_high_minutes,seq(-448-16,482+16,32),labels=seq(-448,482,32))
-
 # Load results from London et al to create survey specific correction factors
 load("all.fit.1.rda")
 # get fixed effect table
 fe=all.fit.1$fixed.effects
-# create design matrix for survey data and add weighting of 1/3 for each of three years
+# create design matrix for survey data and add weighting of 1/3 for each of three years to use the mean value
 dm=cbind(rep(1,nrow(HCdata)),model.matrix(~-1+hour,data=HCdata),model.matrix(~-1+MinFHi.Cat,data=HCdata),
          model.matrix(~-1+MFAug15,data=HCdata),model.matrix(~-1+hour:MFAug15,data=HCdata))
 dm=cbind(dm[,1:56],matrix(1/3,nrow=nrow(dm),ncol=3),dm[,57:ncol(dm)])
@@ -35,10 +36,9 @@ vc_cf=vc_p*cf^4
 HCdata$AbundanceEstimate=cf*HCdata$Count.total
 # Variance-covariance matrix of abundance estimates; assumes binomial for count on diagonal - no covariance on counts
 vc_abundance=HCdata$Count.total%*%t(HCdata$Count.total)*vc_cf+diag(HCdata$AbundanceEstimate*p*(1-p))
-
 # Annual abundance estimates
 HCdata=HCdata[order(HCdata$Year,HCdata$Sitecode),]
-# wts are the number of counts at a site in a year
+# wts are the reciprocal of the number of counts at a site in a year to use the mean at the site
 wts=unlist(sapply(split(HCdata,list(HCdata$Year,formatC(HCdata$Sitecode,digits=3,flag="#"))),function(x) rep(1/nrow(x),nrow(x))))
 HCdata$wts=wts[order(names(wts))]
 # X is a matrix of weights to construct the annual estimates
@@ -51,23 +51,20 @@ X=sapply(sort(unique(HCdata$Year)),function(x){
 AnnualEstimates=as.vector(HCdata$AbundanceEstimate%*%X)
 names(AnnualEstimates)=sort(unique(HCdata$year))
 var_AnnualEstimates=t(X)%*%vc_abundance%*%X
-
 # for 95% confidence interval based on log normal distribution
 SE=sqrt(diag(var_AnnualEstimates))
 CV=SE/AnnualEstimates
 c=exp(1.96*sqrt(log(1+CV^2)))
 UCL=AnnualEstimates*c
 LCL=AnnualEstimates/c
-
 # plot abundance estimates and 95% confidence intervals
-
+pdf(file="HC Abundance.pdf")
 plot(sort(unique(HCdata$Year)),AnnualEstimates,xlab="Year",ylab="Abundance Estimate",type="b",ylim=c(0,6000))
 lines(sort(unique(HCdata$Year)),AnnualEstimates/c,lty=2)
 lines(sort(unique(HCdata$Year)),AnnualEstimates*c,lty=2)
-
+dev.off()
 # Nmin calculation uses z=0.842
 Nmin=AnnualEstimates/exp(.842*sqrt(log(1+CV^2)))
-
 # Output table of results
 HCResults=data.frame(Year=sort(unique(HCdata$Year)),Abundance=round(AnnualEstimates),Stderror=round(SE,1),CV=round(CV,3),LCL=round(LCL),UCL=round(UCL),Nmin=round(Nmin))
 write.csv(HCResults,file="HCResults.csv",row.names=FALSE)
